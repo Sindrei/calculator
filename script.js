00e716a39e4983c454590a292816c2356131e67d
Francois Malan <78906010+Sindrei@users.noreply.github.com>
2022-11-09 00:17:09 +0200

    Added calculator functionality
    Created variables to save calculator values.
    Created event listeners for operator and equals buttons.
    
    Created the following functions:
    * appendNumber(appendValue) - This appends values to the screen.
    * setOperation(operator) - sets the operator to clicked operator.
    * equals() - executes operate to do calculations.

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 let displayValue = "";
-const buttons = document.querySelectorAll(".numbers");
+let firstValue = "";
+let secondValue = "";
+let operand = null;
+
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
 const display = document.querySelector(".number-display");
 
 // Event listeners for number buttons
-buttons.forEach((button) => {
+numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    displayValue += button["value"];
-    display.textContent = displayValue;
+    appendNumber(button.textContent);
   });
 });
+
+// Event listeners for operator buttons
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    setOperation(button.textContent);
+  });
+});
+
+// Event listener for equal sign
+equalsButton.addEventListener("click", () => {
+  equals();
+});
+
+function appendNumber(appendValue) {
+  display.textContent += appendValue;
+  displayValue = display.textContent;
+}
+
+function setOperation(operator) {
+  if (operand === null) {
+    operand = operator;
+    firstValue = displayValue;
+    display.textContent = "";
+  } else if (operand != null) {
+    equals();
+  }
+}
+
+function equals() {
+  if (operand != null) {
+    secondValue = display.textContent;
+    display.textContent = operate(operand, firstValue, secondValue);
+    displayValue = display.textContent;
+    firstValue = display.textContent;
+    operand = null;
+  } else return;
+}
 
 function add(a, b) {
   return a + b;
@@ -27,16 +69,16 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
     case "-":
       return subtract(a, b);
-    case "x":
+    case "×":
       return multiply(a, b);
-    case "/":
+    case "÷":
       return divide(a, b);
-    default:
-      break;
   }
 }
